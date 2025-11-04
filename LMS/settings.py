7d601b9e6f01260802ec10b6994e018623eabaa6
kanyakunjumon <150785632+kanyakunjumon@users.noreply.github.com>
2025-11-04 23:36:02 +0530

    Configure Render database connection

--- LMS/settings.py
+++ LMS/settings.py
@@ -76,16 +76,16 @@ WSGI_APPLICATION = 'LMS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import dj_database_url
+import os
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'LMS_db',
-        'USER':'postgres',
-        'PASSWORD':'postgres',
-        'HOST':'localhost',
-        'PORT':'5432',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 
 # Password validation
