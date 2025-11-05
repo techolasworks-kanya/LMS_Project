6ec2aabf1df3bd078b7fa4f7c22a49d24627f306
kanyakunjumon <150785632+kanyakunjumon@users.noreply.github.com>
2025-11-05 11:33:28 +0530

    custom user deleted

--- LMS_app/models.py
+++ LMS_app/models.py
@@ -3,11 +3,6 @@ from django.contrib.auth.models import User
 from django .contrib.auth.models import AbstractUser
 
 # Create your models here.
-class CustomUser(AbstractUser):
-    username = models.CharField(max_length=150, unique=True)
-    email = models.EmailField(unique=True)
-    def __str__(self):
-        return self.username
 
 
     
