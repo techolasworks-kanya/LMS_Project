26166b85b934c9d9f448ea4e1bb01e768b3446c7
kanyakunjumon <150785632+kanyakunjumon@users.noreply.github.com>
2025-11-05 12:47:49 +0530

    cors added

--- LMS/settings.py
+++ LMS/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ALLOWED_ORIGINS = [ '*']
 ROOT_URLCONF = 'LMS.urls'
 
 TEMPLATES = [
--- LMS_app/models.py
+++ LMS_app/models.py
@@ -3,15 +3,14 @@ from django.contrib.auth.models import User
 from django .contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    def __str__(self):
+        return self.username
 
 
     
-
-
-
-
-
-
 class certfication(models.Model):
     certfication_name = models.CharField(max_length=100)
 
