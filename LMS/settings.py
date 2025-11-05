990bd356ea553487f0804bcffaf0344dfd75df4f
kanyakunjumon <150785632+kanyakunjumon@users.noreply.github.com>
2025-11-05 12:51:14 +0530

    cors added1

--- LMS/settings.py
+++ LMS/settings.py
@@ -51,7 +51,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ALLOWED_ORIGINS = [ '*']
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
 ROOT_URLCONF = 'LMS.urls'
 
 TEMPLATES = [
