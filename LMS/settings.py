6d6fa5ec854a3893c4808cb65abbdfe43fd591c1
kanyakunjumon <150785632+kanyakunjumon@users.noreply.github.com>
2025-11-07 14:54:05 +0530

    Cors issue updated

--- LMS/settings.py
+++ LMS/settings.py
@@ -80,7 +80,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = ['*']
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'LMS.urls'
