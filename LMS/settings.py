0279518fd129f743d13ca2b1ec942d303941297a
kanyakunjumon <150785632+kanyakunjumon@users.noreply.github.com>
2025-12-16 10:49:16 +0530

    pagination commented1

--- LMS/settings.py
+++ LMS/settings.py
@@ -46,8 +46,8 @@ REST_FRAMEWORK = {
     
     ],
 
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 10,
         
     
     'DEFAULT_PERMISSION_CLASSES': (
